2a67ca68302266fd33442f994eec8152011fe04f
Rey David Borrego Perez <rborregop@unicartagena.edu.co>
2026-06-11 15:17:00 -0500

    feat: agregar modelos y repositorios

---
+++ src/main/java/com/udec/unidad2mvc/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.udec.unidad2mvc.repository;
+
+import com.udec.unidad2mvc.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
